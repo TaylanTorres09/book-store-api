5a5b5b7d0ae9532f21d0ee44bdde1c14c9439a52
TaylanTorres09 <taylantorres@gmail.com>
2023-01-11 15:34:55 -0300

    Create controller and service Category

---
+++ src/main/java/com/bookstore/bookstore/controller/CategoryController.java
@@ -0,0 +1,25 @@
+package com.bookstore.bookstore.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bookstore.bookstore.models.Category;
+import com.bookstore.bookstore.service.CategoryService;
+
+@RestController
+@RequestMapping("/category")
+public class CategoryController {
+    
+    @Autowired
+    private CategoryService categoryService;
+
+    @GetMapping("/categories")
+    public List<Category> getCategory() {
+        return categoryService.getCategory();
+    }
+
+}
---
+++ src/main/java/com/bookstore/bookstore/service/CategoryService.java
@@ -0,0 +1,21 @@
+package com.bookstore.bookstore.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bookstore.bookstore.models.Category;
+import com.bookstore.bookstore.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+    
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    public List<Category> getCategory() {
+        return categoryRepository.findAll();
+    }
+
+}
