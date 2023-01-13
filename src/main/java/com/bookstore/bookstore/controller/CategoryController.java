aed0434801c327af2fe15f06b362071624203df5
TaylanTorres09 <taylantorres@gmail.com>
2023-01-13 08:12:23 -0300

    Create Method and route Post for category

--- src/main/java/com/bookstore/bookstore/controller/CategoryController.java
+++ src/main/java/com/bookstore/bookstore/controller/CategoryController.java
@@ -1,16 +1,23 @@
 package com.bookstore.bookstore.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.bookstore.bookstore.dtos.CategoryDTO;
 import com.bookstore.bookstore.models.Category;
 import com.bookstore.bookstore.service.CategoryService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/category")
@@ -36,6 +43,15 @@ public class CategoryController {
                         .map(cat -> new CategoryDTO(cat.getId(), cat.getName(), cat.getDescription()))
                         .toList();
         return categoryDTOs;
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<Category> create(@Valid @RequestBody CategoryDTO categoryDTO) {
+        Category category = this.categoryService.create(categoryDTO);
+        URI uri = ServletUriComponentsBuilder.fromCurrentContextPath().path("/category/{id}").buildAndExpand(category.getId()).toUri();
+        
+        //return uri in headers
+        return ResponseEntity.created(uri).build();
     }
 
 }
--- src/main/java/com/bookstore/bookstore/service/CategoryService.java
+++ src/main/java/com/bookstore/bookstore/service/CategoryService.java
@@ -2,9 +2,11 @@ package com.bookstore.bookstore.service;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.bookstore.bookstore.dtos.CategoryDTO;
 import com.bookstore.bookstore.models.Category;
 import com.bookstore.bookstore.repositories.CategoryRepository;
 import com.bookstore.bookstore.service.exceptions.ObjectNotFound;
@@ -22,6 +24,13 @@ public class CategoryService {
     public Category getByIdCategory(Long id) {
         return this.categoryRepository.findById(id)
                     .orElseThrow(() -> new ObjectNotFound("Category id: " + id + ", not found."));
+    }
+
+    public Category create(CategoryDTO categoryDTO) {
+        categoryDTO.setId(null);
+        Category category = new Category();
+        BeanUtils.copyProperties(categoryDTO, category);
+        return this.categoryRepository.save(category);
     }
 
 }
