7d0e028d1a16f6e7e9d36ccb5ec8286a42722d4e
TaylanTorres09 <taylantorres@gmail.com>
2023-01-14 08:39:07 -0300

    Create Class to Request Book

---
+++ src/main/java/com/bookstore/bookstore/dtos/RequestBook.java
@@ -0,0 +1,34 @@
+package com.bookstore.bookstore.dtos;
+
+import com.bookstore.bookstore.models.Book;
+
+import jakarta.validation.constraints.NotEmpty;
+
+public class RequestBook {
+
+    @NotEmpty
+    private String title;
+
+    @NotEmpty
+    private String author;
+
+    @NotEmpty
+    private String text;
+
+    private Long categoryId;
+
+    public String getTitle() {
+        return title;
+    }
+    
+    public Long getCategoryId() {
+        return categoryId;
+    }
+    public String getAuthor() {
+        return author;
+    }
+    public String getText() {
+        return text;
+    }
+
+}
