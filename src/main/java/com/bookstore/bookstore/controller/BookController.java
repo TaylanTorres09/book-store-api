d9c0ef831e09f084348831e0136135a15243116f
TaylanTorres09 <taylantorres@gmail.com>
2023-01-14 07:55:28 -0300

    Create End Point

--- src/main/java/com/bookstore/bookstore/controller/BookController.java
+++ src/main/java/com/bookstore/bookstore/controller/BookController.java
@@ -3,11 +3,15 @@ package com.bookstore.bookstore.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.bookstore.bookstore.dtos.BookDTO;
 import com.bookstore.bookstore.models.Book;
 import com.bookstore.bookstore.service.BookService;
 
@@ -28,5 +32,12 @@ public class BookController {
         return this.bookService.getByIdBook(id);
     }
     
+    @GetMapping
+    public ResponseEntity<List<BookDTO>> findByCategory(@RequestParam(value = "category", defaultValue = "0") Long idCategory) {
+        List<Book> books = this.bookService.findByCategory(idCategory);
+        List<BookDTO> bookDTOs = books.stream().map(book -> new BookDTO(book)).toList();
+
+        return new ResponseEntity<List<BookDTO>>(bookDTOs, HttpStatus.OK);
+    }
     
 }
