3dfdcdbae618b95c8beb4f7152b13c3984c32725
TaylanTorres09 <taylantorres@gmail.com>
2023-01-15 07:23:59 -0300

    Custom validation messages

--- src/main/java/com/bookstore/bookstore/dtos/CategoryDTO.java
+++ src/main/java/com/bookstore/bookstore/dtos/CategoryDTO.java
@@ -1,5 +1,7 @@
 package com.bookstore.bookstore.dtos;
 
+import org.hibernate.validator.constraints.Length;
+
 import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -10,10 +12,12 @@ public class CategoryDTO {
     
     private Long id;
 
-    @NotEmpty
+    @NotEmpty(message = "Campo NOME é obrigatório")
+    @Length(min = 3, max = 100, message = "O campo NOME deverá ter entre 3 a 100 caracteres")
     private String name;
 
-    @NotEmpty
+    @NotEmpty(message = "Campo DESCRIÇÃO é obrigatório")
+    @Length(min = 3, max = 200, message = "O campo DESCRIÇÃO deverá ter entre 3 a 200 caracteres")
     private String description;
 
 }
--- src/main/java/com/bookstore/bookstore/dtos/RequestBook.java
+++ src/main/java/com/bookstore/bookstore/dtos/RequestBook.java
@@ -1,16 +1,21 @@
 package com.bookstore.bookstore.dtos;
 
+import org.hibernate.validator.constraints.Length;
+
 import jakarta.validation.constraints.NotEmpty;
 
 public class RequestBook {
 
-    @NotEmpty
+    @NotEmpty(message = "Campo TITULO é obrigatório")
+    @Length(min = 3, max = 50, message = "O campo TITULO deverá ter entre 3 a 50 caracteres")
     private String title;
 
-    @NotEmpty
+    @NotEmpty(message = "Campo NOME do AUTOR é obrigatório")
+    @Length(min = 3, max = 50, message = "O campo NOME do AUTOR deverá ter entre 3 a 200 caracteres")
     private String author;
 
-    @NotEmpty
+    @NotEmpty(message = "Campo TEXTO é obrigatório")
+    @Length(min = 10, max = 1000000, message = "O campo TEXTO deverá ter entre 10 a 1.000.000 caracteres")
     private String text;
 
     private Long categoryId;
