43bd824806758b5fda8197b5518298aa4a156eee
TaylanTorres09 <taylantorres@gmail.com>
2023-01-16 06:50:49 -0300

    Finalized validation adjustment

--- src/main/java/com/bookstore/bookstore/controller/exceptions/ControllerExceptionHandler.java
+++ src/main/java/com/bookstore/bookstore/controller/exceptions/ControllerExceptionHandler.java
@@ -2,6 +2,8 @@ package com.bookstore.bookstore.controller.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -21,6 +23,16 @@ public class ControllerExceptionHandler {
     @ExceptionHandler(DataIntegrityViolationException.class)
     public ResponseEntity<StandardError> dataIntegrityViolationException(DataIntegrityViolationException e, ServletRequest request) {
         StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), e.getMessage());
+        return new ResponseEntity<StandardError>(error, HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<StandardError> validationError(MethodArgumentNotValidException e, ServletRequest request) {
+        ValidationError error = new ValidationError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), "Erro na validação dos campos");
+        
+        for(FieldError x: e.getBindingResult().getFieldErrors()) {
+            error.addErrors(x.getField(), x.getDefaultMessage());
+        }
+
         return new ResponseEntity<StandardError>(error, HttpStatus.BAD_REQUEST);
     }
 
