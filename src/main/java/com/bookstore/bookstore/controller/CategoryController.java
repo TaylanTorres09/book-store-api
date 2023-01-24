634d18c3c2df37610db9187336ea09bc7fe95e95
TaylanTorres09 <taylantorres@gmail.com>
2023-01-24 13:16:59 -0300

    Enable CrossOrigin for all requests

--- src/main/java/com/bookstore/bookstore/controller/CategoryController.java
+++ src/main/java/com/bookstore/bookstore/controller/CategoryController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ import com.bookstore.bookstore.service.CategoryService;
 
 import jakarta.validation.Valid;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/category")
 public class CategoryController {
